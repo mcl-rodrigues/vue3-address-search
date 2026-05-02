e4e4dce564ad8e116db4bc62982b77b805767f48
marcelo <marina19.software@gmail.com>
2026-05-02 15:38:54 -0300

    chore: setup inicial do projeto com vue router e estrutura base

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Home.vue'
+import AddressesView from '../views/Addresses.vue'
+import AboutView from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/enderecos',
+    name: 'addresses',
+    component: AddressesView,
+  },
+  {
+    path: '/sobre',
+    name: 'about',
+    component: AboutView,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
